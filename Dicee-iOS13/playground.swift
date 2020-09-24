b427eca555efdf99e5ddac77704c251f6132bab0
Wang, Paul <paul.wang@rakuten.com>
2020-09-24 00:32:58 -0700

    Swift String Interpolation

---
+++ Dicee-iOS13/playground.swift
@@ -0,0 +1,20 @@
+//
+//  playground.swift
+//  Dicee-iOS13
+//
+//  Created by Wang, Paul on 9/24/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class PlayGround {
+    
+    /*
+     String Interpolation, \(code goes in here)
+     */
+    func stringInterpolation() {
+        print("what is 2+7? \(2+5)")
+    }
+
+}
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print("what is 2+7? \(2+5)")
