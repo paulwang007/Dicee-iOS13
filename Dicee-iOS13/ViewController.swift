201eec273df4547bc36aabd94f340f4a2acec75f
paulwang007 <5761415+paulwang007@users.noreply.github.com>
2020-09-24 23:53:52 -0700

    test

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     
     // @IBAction
     @IBAction func rollDice(_ sender: UIButton) {
+        
         // [list items][index]
         diceOne?.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo")][1]
     }
